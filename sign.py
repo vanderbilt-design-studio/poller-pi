c126c259ab1e81c31f65a5f7139b9f29b208b91e
Sameer Puri <sameer@users.noreply.github.com>
2019-09-10 23:17:16 -0500

    Add explicit internal pull down resistors for switch pins

--- sign.py
+++ sign.py
@@ -4,6 +4,8 @@ def sign_setup():
     wiringpi.wiringPiSetupGpio()
     for input in [17, 18, 27]:
         wiringpi.pinMode(input, wiringpi.INPUT)
+    for switch_pin in [17, 27]:
+        wiringpi.pullUpDnControl(switch_pin, wiringpi.PUD_DOWN)
 
 
 
