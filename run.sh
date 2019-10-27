40d2e0a1a9ce483fba7f68b2f2e9bf4be91a5234
Sameer Puri <sameer@users.noreply.github.com>
2019-10-27 12:10:55 -0500

    Pull API key from heroku iff heroku command available

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-X_API_KEY=abcd python /root/poller-pi/main.py
+
+# https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+X_API_KEY=`command -v heroku 2>/dev/null 1>&2 && heroku config:get X_API_KEY -a vanderbilt-design-studio || echo $X_API_KEY`
+
+X_API_KEY=$X_API_KEY python "$DIR/main.py"
