6a3aabeb669193c2cee93743d8348cffdf874926
Sameer Puri <sameer@users.noreply.github.com>
2019-10-27 12:13:57 -0500

    Add explanation for X_API_KEY script

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 # https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# https://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script
 X_API_KEY=`command -v heroku 2>/dev/null 1>&2 && heroku config:get X_API_KEY -a vanderbilt-design-studio || echo $X_API_KEY`
 
 X_API_KEY=$X_API_KEY python "$DIR/main.py"
